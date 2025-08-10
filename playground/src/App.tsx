3d1358e98445e1b3736471b104de85eef34a104b
savageKarl <2443005004@qq.com>
2025-08-10 22:05:31 +0800

    feat: complete basic feature
    - config CICD
    - config semantic-release
    - change unit test for useStore hook

---
+++ playground/src/App.tsx
@@ -0,0 +1,23 @@
+import { defineStore } from '../../src'
+
+export const useCounterStore = defineStore('counter', {
+  state: () => {
+    return { count: 0 }
+  },
+  actions: {
+    increment() {
+      this.count++
+    }
+  }
+})
+
+export function App() {
+  const store = useCounterStore()
+  console.log('number')
+  return (
+    <>
+      <h1>{store.count}</h1>
+      <button onClick={() => store.increment()}>increate</button>
+    </>
+  )
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { defineStore } from './defineStore'
+export { createPinia, pinia, setActivePinia } from './pinia'
+
+export type {
+  DefineStoreOptionsBase,
+  PiniaCustomProperties,
+  PiniaCustomStateProperties,
+  PiniaPlugin,
+  PiniaPluginContext,
+  StateTree,
+  Store
+} from './types'
