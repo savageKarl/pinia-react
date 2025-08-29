3c1e9422d6fa428f7f4e1d6a59a5257733b96103
savageKarl <2443005004@qq.com>
2025-08-29 09:51:27 +0800

    chore: change rspress default lang to en

--- packages/docs/rspress.config.ts
+++ packages/docs/rspress.config.ts
@@ -2,7 +2,7 @@ import * as path from 'node:path'
 import { defineConfig } from 'rspress/config'
 
 export default defineConfig({
-  lang: 'zh',
+  lang: 'en',
   root: path.join(__dirname, 'docs'),
   title: 'Pinia-React',
   // icon: '/rspress-icon.png',
